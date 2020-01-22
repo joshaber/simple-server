3ff7d180dc641f8a56d841fd33d971ae53454a15
joshaber <joshaber@gmail.com>
2020-01-22 13:33:31 -0500

    Log requests

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  console.log(`${new Date()} ${req.method} ${req.path}`);
+  res.send("Hello World!");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
